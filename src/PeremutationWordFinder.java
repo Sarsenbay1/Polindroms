6fd419087ea43ff88149aee3b8413b6190bd581b
maksat sarsenbaev <88890025m@gmail.com>
2024-03-16 01:47:41 +0600

    fix

--- src/PeremutationWordFinder.java
+++ src/PeremutationWordFinder.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.HashMap;
 
 public class PeremutationWordFinder implements IWordFinder {
@@ -7,28 +6,25 @@ public class PeremutationWordFinder implements IWordFinder {
         // Разбиваем входную строку на слова и сохраняем их в массив _words
         String[] _words = this.textFormatting(_text);
         // Создать Map для хранения найденных пар
-        HashMap<String, String> permutations = new HashMap<>();
+        HashMap<String, String> _permutations = new HashMap<String, String>();
 
         // Поиск перестановок слов
         for (Integer i = 0; i < _words.length; i++) {
             for (Integer j = 0; j < _words.length; j++) {
                 if (i != j) {
-                    // Преобразуем слова в массивы символов и сортируем их
-                    char[] word1 = _words[i].toCharArray();
-                    char[] word2 = _words[j].toCharArray();
-                    Arrays.sort(word1);
-                    Arrays.sort(word2);
-                    // Если отсортированные массивы символов равны и первое слово не содержится в
+
                     // Map, то добавляем пару в Map
-                    if (Arrays.equals(word1, word2) && !permutations.containsKey(_words[i])
-                            && !permutations.containsValue(_words[i])) {
-                        permutations.put(_words[i], _words[j]);
+                    // System.out.println(_word1 + " " + _word2);
+                    if (_words[i].equals(new StringBuilder(_words[j]).reverse().toString())
+                            && !_permutations.containsKey(_words[i])
+                            && !_permutations.containsValue(_words[i])) {
+                        _permutations.put(_words[i], _words[j]);
                     }
                 }
 
             }
         }
-        return permutations;
+        return _permutations;
     }
 
     protected String[] textFormatting(String _text) {
--- src/PolyndromeFinder.java
+++ src/PolyndromeFinder.java
@@ -1,24 +1,23 @@
 import java.util.ArrayList;
-// import java.util.List;
 
 public class PolyndromeFinder extends PeremutationWordFinder {
     public String[] findPolydrome(String _defaultText) {
         String[] _words = this.textFormatting(_defaultText);
 
-        ArrayList<String> _palindromeWords = new ArrayList<String>();
+        ArrayList<String> _polyndromeWords = new ArrayList<String>();
 
         for (String _word : _words) {
-            if (isPalindrome(_word) && !_palindromeWords.contains(_word)) { // Проверка слова на палиндром
-                _palindromeWords.add(_word);
+            if (isPolyndrome(_word) && !_polyndromeWords.contains(_word)) { // Проверка слова на палиндром
+                _polyndromeWords.add(_word);
             }
         }
-        return _palindromeWords.toArray(new String[_palindromeWords.size()]);// Преобразование списка в массив и
+        return _polyndromeWords.toArray(new String[_polyndromeWords.size()]);// Преобразование списка в массив и
                                                                              // возвращение его
 
     }
 
-    private static boolean isPalindrome(String _word) {
-        return _word.equals(new StringBuilder(_word).reverse().toString()); // Проверка на палиндром
+    private static boolean isPolyndrome(String _word) {
+        return _word.equals(new StringBuilder(_word).reverse().toString()); // Проверка на полиндром
 
     }
 }
